b466a5a43cf510662fa7485c85ac03351d1e3ffa
zhadkom <borntodievych@gmail.com>
2020-06-19 20:18:41 +0300

    Server side auth

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,45 @@
 const express = require('express')
+const DB = require('../db/db')
 const bodyParser = require('body-parser')
-const cors = require('cors')
-const morgan = require('morgan')
+const bcrypt = require('bcrypt')
 const config = require('../config/config,js')
 
+const db = new DB('sqlitedb')
 const app = express()
+const router = express.Router()
 
-app.use(morgan('combined'))
-app.use(bodyParser.json())
-app.use(cors())
+router.use(bodyParser.urlencoded({ extended: false }))
+router.use(bodyParser.json())
+
+// CORS middleware
+const allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  next()
+}
+
+app.use(allowCrossDomain)
+
+router.post('/register', function (req, res) {
+  db.insert([
+    req.body.name,
+    req.body.email,
+    bcrypt.hashSync(req.body.password, 8),
+  ]),
+    function (err) {
+      if (err) {
+        return res.status(500).send('That was a problem registering the user')
+      }
+      db.selectByEmail(req.body.email, (err, user) => {
+        if (err) {
+          return res.status(500).send('That was a problem getting user')
+        }
+        let token = jwt.sign({ id: user.id }, config.secret, { expires: 86400 })
+        res.status(200).send({ auth: true, token: token, user: user })
+      })
+    }
+})
 
 app.get('/posts', (req, res) => {
   res.send([
@@ -18,6 +49,8 @@ app.get('/posts', (req, res) => {
     },
   ])
 })
+
+app.use(router)
 
 app.listen(process.env.PORT || config.port, () => {
   console.log('Server start on port: ' + config.port)
